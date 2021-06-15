b731f63bea4d9b3029a6fc7c3b3f7fbe963deaad
jakubmoryc <moryckuba@gmail.com>
2021-06-15 10:52:13 +0200

    added axios and comments

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '@material-ui/core/Container';
 import { DataGrid } from '@material-ui/data-grid';
 import './list.css'
+import axios from 'axios';
 
 export default function List() {
 
+    const [planetData, setPlanetData] = useState([])
+
+    //Get planets from the API
+    useEffect(() => {
+        axios
+        .get('https://swapi.dev/api/planets')
+        .then(json => console.log(json))
+    }, [])
+
+    //Grid setup
     const rows = [
         { id: 1, col1: "Hello", col2: "World" },
         { id: 2, col1: "XGrid", col2: "is Awesome" },
