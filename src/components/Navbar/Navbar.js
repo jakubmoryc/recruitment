07862c65ae305f44f5164d801db21fe25c793a23
jakubmoryc <moryckuba@gmail.com>
2021-06-15 10:06:42 +0200

    Added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>
+                Planet List
+            </h1>
+        </nav>
+    )
+}
